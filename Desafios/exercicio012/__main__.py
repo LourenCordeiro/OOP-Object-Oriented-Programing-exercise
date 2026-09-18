b1ff54885ae5866974ae872b1bdb5c90452707e4
Louren Mayara <cordeiroalves.1994@outlook.com>
2026-09-18 18:01:52 -0300

    polimorfismo1

---
+++ Desafios/exercicio012/__main__.py
@@ -0,0 +1,24 @@
+from classes import *
+
+
+def main():
+
+    a = Cachorro("Sansa")
+    b = Gato("Ursula")
+    c = Pato("Donald")
+    d = Galinha("Pretinha")
+    e = Spitz("Farofa")
+    f = Pitbull("Thor")
+
+    a.emitir_som()
+    b.emitir_som()
+    c.emitir_som()
+    d.emitir_som()
+    e.emitir_som()
+    f.emitir_som()
+
+
+
+
+if __name__ == "__main__":
+    main()
