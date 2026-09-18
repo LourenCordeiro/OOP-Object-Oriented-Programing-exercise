b36ba8a26919d29cf012823ec90b2108b43fc827
Louren Mayara <cordeiroalves.1994@outlook.com>
2026-09-18 10:23:25 -0300

    FuctionOverload-OperatorOverload

---
+++ Desafios/exercicio011(polimorfismo)/__main__.py
@@ -0,0 +1,10 @@
+#Exemplo de Function Overload
+print(len("Louren Mayara"))
+print(len(["Curso", "Python"]))
+print(len({"x": "y", "a":"b"}))
+
+#Exemplo de Operator Overload
+print(+12)
+print(5+5)
+print("Poli" + "morfismo")
+print([1,2,3] + [4,5,6])
